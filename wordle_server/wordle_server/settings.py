4230b39d1ae06303af44ae55414e80c2b446de80
hieuttblackpink <trantronghieu369963@gmail.com>
2024-11-22 15:38:14 +0700

    config deploy Railway

--- wordle_server/wordle_server/settings.py
+++ wordle_server/wordle_server/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*=4+sw%_k1-f1p!!hz8@i@+)(k_lf4o+s3%$v0q_s4)@6b9=hp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['hieutt_wordle_server.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['hieutt_wordle_server.onrender.com', '127.0.0.1', 'hieutt_wordle_server.up.railway.app']
 
 
 # Application definition
